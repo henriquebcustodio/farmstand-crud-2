56d2c91ddac8b3eb73d577dcc7ac707a8f6b0f73
henriquebcustodio <77708400+henriquebcustodio@users.noreply.github.com>
2021-08-16 18:55:24 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const Product = require('./models/products');
+const Farm = require('./models/farm');
+
+mongoose.connect('mongodb://localhost:27017/farmStandTake2', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MONGO Connection Open');
+    })
+    .catch(err => {
+        console.log('MONGO Connection Error');
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// FARM ROUTES
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms });
+});
+
+// New farm route
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new');
+});
+
+// Farm creation
+app.post('/farms', async (req, res) => {
+    const newFarm = new Farm(req.body);
+    await newFarm.save();
+    res.redirect('/farms');
+});
+
+// Farm show page
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    console.log(farm);
+    res.render('farms/details', { farm });
+});
+
+// New product inside a farm form
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm });
+});
+
+// Create new product in a farm
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`);
+});
+
+
+// PRODUCT ROUTES
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+// Index page
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+});
+
+// New product form
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories });
+});
+
+// Product creation
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+});
+
+// Details page
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id).populate('farm', 'name');
+    res.render('products/details', { product });
+});
+
+// Edit form
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+});
+
+// Edit product
+app.put('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+});
+
+app.delete('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+});
